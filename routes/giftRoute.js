3aa7ca851d41883c8548bde12af19bf860603846
dasi254844 <dasi254844@gmail.com>
2025-01-23 16:49:40 +0200

    Update giftRoute.js

--- routes/giftRoute.js
+++ routes/giftRoute.js
@@ -1,19 +1,18 @@
-import { Router } from "express"
+import {Router} from "express"
 import { addGift, deleteGiftById, getAllGiftOutOfStock, getAllGifts, getGiftById, updateGift } from "../controllers/giftController.js"
 
 const giftRoute = Router()
 
-giftRoute.get("", getAllGifts)
+giftRoute.get("",getAllGifts)
 
-giftRoute.get("/:id", getGiftById)
+giftRoute.get("/get/:id", getGiftById)
 
-giftRoute.get("/Out_of_stock", getAllGiftOutOfStock)
+giftRoute.get("/get_out_of_stock", getAllGiftOutOfStock)
 
 giftRoute.delete("/:id", deleteGiftById)
 
-giftRoute.post("", addGift)
+giftRoute.post("",addGift)
 
 giftRoute.put("/:id", updateGift)
-
 
 export default giftRoute;
