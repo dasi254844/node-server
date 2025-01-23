2b4cfb8b90e583250d5abcfc04eb461b91ea9317
dasi254844 <dasi254844@gmail.com>
2025-01-23 16:50:52 +0200

    Update orderRoute.js

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,23 +1,22 @@
-import { Router } from "express"
-import { getAllOrders, getOrderById, getAllOrdersInDate, getOrdersFromUserById, deleteOrder, addOrder, updateOrder, sendingOrderUpdate } from "../controllers/orderController.js"
+import {Router} from "express"
+import { getAllOrders, getOrderById ,getAllOrdersInDate, getOrdersFromUserById ,deleteOrder,addOrder,updateOrder, sendingOrderUpdate} from "../controllers/orderController.js"
 
 const orderRoute = Router();
 
 orderRoute.get("", getAllOrders);
 
-orderRoute.get("/:id", getOrderById);
+orderRoute.get("/get/:id", getOrderById);
 
-orderRoute.get("/target_date/:date", getAllOrdersInDate);
+orderRoute.get("/get_target_date/:date", getAllOrdersInDate);
 
-orderRoute.get("user/:id", getOrdersFromUserById);
+orderRoute.get("/get_user/:id", getOrdersFromUserById);
 
 orderRoute.delete("/:id", deleteOrder);
 
-orderRoute.post("", addOrder);
+orderRoute.post("",addOrder);
 
-orderRoute.put("/:id", updateOrder);
+orderRoute.put("/update/:id", updateOrder);
 
-orderRoute.put("/sending/:id", sendingOrderUpdate);
-
+orderRoute.put("/update_sending/:id", sendingOrderUpdate);
 
 export default orderRoute;
