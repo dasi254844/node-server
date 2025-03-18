d5aca4459410b3b4b15c3931a578aa635dc8876f
Dasi_R <dasi254844@gmail.com>
2025-03-18 11:59:47 +0200

    server1

--- controllers/userController.js
+++ controllers/userController.js
@@ -123,8 +123,8 @@ export const getUserByLogin = async (req, res) => {
     if (!email || !password)
         return res.status(400).json({ title: "error in get by login", message: "missing details" });
     try {
-        let data = await Users.findOne({ password, email });
-        if (!data)
+        let data = await Users.find({ password, email });
+        if (data.length == 0)
             return res.status(400).json({ title: "cannot get by login", message: "no user with such details" });
         data.password = undefined;
         data.token = generateToken(data);
