56b96a507791d6af8a4f00caa26a41eeb1fafd99
Dasi_R <dasi254844@gmail.com>
2025-03-18 12:08:41 +0200

    server1234

--- models/userModel.js
+++ models/userModel.js
@@ -2,8 +2,8 @@ import { Schema, model } from "mongoose"
 
 const userSchema = Schema({
     userName: { type: String, required: true },
-    password: { type: String, required: true, unique: true, "pattern": "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$" },
-    email: { type: String, required: true, "pattern": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" },
+    password: { type: String, required: true, "pattern": "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$" },
+    email: { type: String, required: true, unique: true, "pattern": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" },
     phone: { type: String, required: true },
     registration_date: { type: Date, default: Date.now },
     role: { type: String, enum: ['admin', 'user'], required: true, default: "user" }
